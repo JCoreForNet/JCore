17a237241779ca450ec63e310b9ed52bc4f7b80d
JakeChan <13697758238@163>
2018-06-25 18:03:02 +0800

    Setup .netcore 2.0 project environment and show home page successfully

--- JCore/Startup.cs
+++ JCore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace JCore
 {
@@ -26,8 +27,10 @@ namespace JCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+                
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -41,6 +44,8 @@ namespace JCore
             app.UseStaticFiles();
 
             app.UseMvc();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
